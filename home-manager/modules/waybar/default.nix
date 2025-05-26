05ae54a23c48b5a1b222da72b85ea5cf9d7a5ca1
WilOl <maximpaskoserhi@gmail.com>
2025-05-26 13:20:06 +0300

    edited waybar and wallpaper

--- home-manager/modules/waybar/default.nix
+++ home-manager/modules/waybar/default.nix
@@ -9,44 +9,24 @@
         height = 30;
         modules-left = ["hyprland/workspaces"];
         modules-center = ["hyprland/window"];
-        modules-right = ["hyprland/language" "custom/weather" "pulseaudio" "battery" "clock" "tray"];
+        modules-right = ["hyprland/language" "pulseaudio" "battery" "clock" "tray"];
         "hyprland/workspaces" = {
           disable-scroll = true;
           show-special = true;
           special-visible-only = true;
           all-outputs = false;
           format = "{icon}";
-          format-icons = {
-            "1" = "";
-            "2" = "";
-            "3" = "";
-            "4" = "";
-            "5" = "";
-            "6" = "";
-            "7" = "";
-            "8" = "";
-            "9" = "";
-            "magic" = "";
-          };
 
           persistent-workspaces = {
-            "*" = 9;
+            "*" = 8;
           };
         };
 
         "hyprland/language" = {
           format-en = "🇺🇸";
-          format-ru = "🇷🇺";
-          format-he = "🇮🇱";
+          format-ua = "🇺🇦";
           min-length = 5;
           tooltip = false;
-        };
-
-        "custom/weather" = {
-          format = " {} ";
-          exec = "curl -s 'wttr.in/Tashkent?format=%c%t'";
-          interval = 300;
-          class = "weather";
         };
 
         "pulseaudio" = {
